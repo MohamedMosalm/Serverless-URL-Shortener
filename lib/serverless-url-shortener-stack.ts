b0afe3aeb829ed01d8e5f30ae98c7099de3ce1fb
Mohamed Mosalm <mohamed.mosalm2002@gmail.com>
2025-08-06 20:17:21 +0300

    feat: Implement serverless URL shortener stack with API-Gateway, lambda, and DynamoDB integration

--- lib/serverless-url-shortener-stack.ts
+++ lib/serverless-url-shortener-stack.ts
@@ -1,16 +1,54 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import path from "path";
 
 export class ServerlessUrlShortenerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const shortnerFn = new cdk.aws_lambda.Function(this, "ShortnerFunction", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "handler.handler",
+      code: lambda.Code.fromAsset(path.join(__dirname, "../lambda")),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessUrlShortenerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const urlShortenerApi = new cdk.aws_apigateway.RestApi(
+      this,
+      "UrlShortenerApi",
+      {
+        restApiName: "Url Shortener Service",
+        description: "This service shortens URLs.",
+        defaultCorsPreflightOptions: {
+          allowOrigins: apigateway.Cors.ALL_ORIGINS,
+          allowMethods: apigateway.Cors.ALL_METHODS,
+          allowHeaders: [
+            "Content-Type",
+            "X-Amz-Date",
+            "Authorization",
+            "X-Api-Key",
+          ],
+        },
+      }
+    );
+
+    urlShortenerApi.root
+      .addResource("create")
+      .addMethod("POST", new apigateway.LambdaIntegration(shortnerFn));
+    urlShortenerApi.root
+      .addResource("{short_url}")
+      .addMethod("GET", new apigateway.LambdaIntegration(shortnerFn));
+
+    const dtable = new cdk.aws_dynamodb.TableV2(this, "UrlShortenerTable", {
+      tableName: "urlMapping",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      billing: cdk.aws_dynamodb.Billing.onDemand(),
+      partitionKey: {
+        name: "short_url",
+        type: cdk.aws_dynamodb.AttributeType.STRING,
+      },
+    });
+    dtable.grantReadWriteData(shortnerFn);
   }
 }
